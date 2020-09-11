2851af47ba320f5e201d1ee96ffffb71a1248b95
Erik Sanne <erik@eriksanne.com>
2020-09-11 15:43:57 +0100

    Add search (WIP)

--- client/src/components/Modules/ModuleFilters.vue
+++ client/src/components/Modules/ModuleFilters.vue
@@ -5,29 +5,30 @@
       Filters
     </v-card-title>
     <v-card-text>
-      <!--            <ApolloQuery v-for="(item, i) in filterOptions"-->
-      <!--                         :key="i"-->
-      <!--                         :query="gql => gql`query ($fieldName: String!) {-->
-      <!--                            moduleDistinct(fieldName: $fieldName)-->
-      <!--                         }`"-->
-      <!--                         :variables="{ fieldName: item.fieldName }"-->
-      <!--                         :update="data => data.moduleDistinct"-->
-      <!--            >-->
-      <!--                <template v-slot="{ result: {data}}">-->
-      <!--                    <div v-if="data" class="result apollo">-->
-      <!--                        <v-autocomplete-->
-      <!--                                v-model="item.selection"-->
-      <!--                                :items="data"-->
-      <!--                                :label="item.label"-->
-      <!--                                multiple-->
-      <!--                                chips-->
-      <!--                                @input="changeFilterOptions"-->
-      <!--                        >-->
-      <!--                        </v-autocomplete>-->
-      <!--                    </div>-->
-      <!--                </template>-->
-      <!--            </ApolloQuery>-->
-
+      <ApolloQuery v-for="(item, i) in filterOptions"
+                   :key="i"
+                   :query="gql => gql`query ${item.root}($fieldName: ${item.fieldType}) {
+                                  ${item.root}(distinct_on: $fieldName) {
+                                    ${item.fieldName}
+                                  }
+                               }`"
+                   :variables="{ fieldName: item.fieldName }"
+                   :update="data => data[`${item.root}`]"
+      >
+        <template v-slot="{ result: {data}}">
+          <div v-if="data" class="result apollo">
+            <v-autocomplete
+                v-model="item.selection"
+                :items="data"
+                :item-text="item.fieldName"
+                :label="item.label"
+                multiple
+                chips
+                @input="changeFilterOptions"
+            ></v-autocomplete>
+          </div>
+        </template>
+      </ApolloQuery>
     </v-card-text>
   </v-card>
 </template>
@@ -36,38 +37,31 @@
 export default {
   name: "ModuleFilters",
   data: () => ({
-    filter: {},
     filterOptions: [
-      {label: 'School', fieldName: 'school', selection: []},
-      {label: 'Subject', fieldName: 'subject_area_group', selection: []},
-      {label: 'Location of delivery', fieldName: 'teaching_instances.delivery_location', selection: []},
-      {label: 'Trimester', fieldName: 'teaching_instances.trimester', selection: []},
-      {label: 'Term', fieldName: 'teaching_instances.term', selection: []},
-      {label: 'Mode of delivery', fieldName: 'teaching_instances.delivery_mode', selection: []},
+      {label: 'School', root: 'modules', fieldName: 'school', fieldType: '[modules_select_column!]', selection: []},
+      {label: 'Subject', root: 'modules', fieldName: 'subject_area_group', fieldType: '[modules_select_column!]', selection: []},
+      {label: 'Location of delivery', root: 'teaching_instances', fieldName: 'delivery_location', fieldType: '[teaching_instances_select_column!]', selection: []},
+      {label: 'Trimester', root: 'teaching_instances', fieldName: 'trimester', fieldType: '[teaching_instances_select_column!]', selection: []},
+      {label: 'Term', root: 'teaching_instances', fieldName: 'term', fieldType: '[teaching_instances_select_column!]', selection: []},
+      {label: 'Mode of delivery', root: 'teaching_instances', fieldName: 'delivery_mode', fieldType: '[teaching_instances_select_column!]', selection: []},
     ]
   }),
   methods: {
     changeFilterOptions: function () {
-      // let filter = { AND: []};
-      // this.filterOptions.forEach(item => {
-      //     if(item.selection.length > 0){
-      //         let fieldObject = {OR: []}
-      //         item.selection.forEach(selection => {
-      //             let selectionObject = {};
-      //             let splitFieldName = item.fieldName.split(".");
-      //             if(splitFieldName.length === 2){
-      //                 selectionObject[splitFieldName[0]] = {};
-      //                 selectionObject[splitFieldName[0]][splitFieldName[1]] = selection;
-      //             }else{
-      //                 selectionObject[splitFieldName[0]] = selection;
-      //             }
-      //             fieldObject.OR.push(selectionObject)
-      //         })
-      //         filter.AND.push(fieldObject)
-      //     }
-      // });
-      let filter = ""
-      this.$emit('input', filter)
+      let filter = {};
+      this.filterOptions.forEach(item => {
+        if(item.selection.length > 0) {
+          if(item.root === 'modules') {
+            if(!filter[item.fieldName]) filter[item.fieldName] = {}
+            filter[item.fieldName]['_in'] = item.selection
+          } else {
+            if(!filter[item.root]) filter[item.root] = {}
+            if(!filter[item.root][item.fieldName]) filter[item.root][item.fieldName] = {}
+            filter[item.root][item.fieldName]['_in'] = item.selection
+          }
+        }
+      });
+      this.$emit('filterChanged', filter)
     }
   }
 }
