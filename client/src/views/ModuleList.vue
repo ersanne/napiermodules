234f7789ff5b4ba011fb52b7f3f10eaa0191de76
Erik Sanne <erik@eriksanne.com>
2020-09-11 13:57:03 +0100

    Connected new graphql endpoint and adapted module list and module page

--- client/src/views/ModuleList.vue
+++ client/src/views/ModuleList.vue
@@ -1,96 +1,99 @@
 <template>
-    <v-container fluid>
+  <v-container fluid>
+    <v-row>
+      <v-col md="1">
+      </v-col>
+      <v-col cols="12"
+             md="3">
         <v-row>
-            <v-col md="1">
-            </v-col>
-            <v-col cols="12"
-                   md="3">
-                <v-row>
-                    <v-col cols="12">
-                        <ModuleFilters v-model="filter" @change="filterChanged"></ModuleFilters>
-                    </v-col>
-                </v-row>
-            </v-col>
-            <v-col cols="12"
-                   md="8">
-                <v-row>
-                    <v-col cols="12"
-                    md="7">
-                        <v-text-field
-                                label="Type to search"
-                        ></v-text-field>
-                    </v-col>
-                    <v-col cols="6"
-                    md="2">
-                        <v-select
-                                v-model="sort"
-                                :items="sortOptions"
-                                label="Sort by"></v-select>
-                    </v-col>
-                    <v-col cols="6"
-                    md="1">
-                        <v-select
-                                v-model="pageSize"
-                                :items="pageSizes"
-                        label="Show per page"></v-select>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col cols="10">
-                        <ApolloQuery :query="gql => gql`query modulePagination ($page: Int!, $pageSize: Int!, $filter: FilterFindManyModuleInput) {
-                    modulePagination(page: $page, perPage: $pageSize, filter: $filter) {
-                        items {
-                            _id
-                            module_title
+          <v-col cols="12">
+            <ModuleFilters v-model="filter" @change="filterChanged"></ModuleFilters>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="12"
+             md="8">
+        <v-row>
+          <v-col cols="12"
+                 md="7">
+            <v-text-field
+                label="Type to search"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="6"
+                 md="2">
+            <v-select
+                v-model="sort"
+                :items="sortOptions"
+                label="Sort by"></v-select>
+          </v-col>
+          <v-col cols="6"
+                 md="1">
+            <v-select
+                v-model="pageSize"
+                :items="pageSizes"
+                label="Show per page"></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="10">
+            <ApolloQuery :query="gql => gql`query modules($offset: Int!, $limit: Int!, $filter: String!) {
+                    modules(offset: $offset, limit: $limit) {
+                            code
+                            title
                             scqf_level
                             scqf_credit_value
                             school
                             subject_area_group
-                        }
                     }
                 }`"
-                                     :variables="{page: page, pageSize: pageSize, filter: filter}"
-                                     :update="data => data.modulePagination.items"
-                        >
-                            <template v-slot="{ result: {data}}">
-                                <div v-if="data" class="result apollo">
-                                    <v-row v-for="(module, i) in data"
-                                           :key="i">
-                                        <v-col cols="12">
-                                            <Module :module="module"></Module>
-                                        </v-col>
-                                    </v-row>
-                                </div>
-                            </template>
-                        </ApolloQuery>
+                         :variables="{offset: offset, limit: pageSize, filter: filter}"
+                         :update="data => data.modules"
+            >
+              <template v-slot="{ result: {data}}">
+                <div v-if="data" class="result apollo">
+                  <v-row v-for="(module, i) in data"
+                         :key="i">
+                    <v-col cols="12">
+                      <Module :module="module"></Module>
                     </v-col>
-                </v-row>
-            </v-col>
+                  </v-row>
+                </div>
+              </template>
+            </ApolloQuery>
+          </v-col>
         </v-row>
-    </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    import Module from "@/components/Modules/ModuleCard";
-    import ModuleFilters from "@/components/Modules/ModuleFilters";
+import Module from "@/components/Modules/ModuleCard";
+import ModuleFilters from "@/components/Modules/ModuleFilters";
 
-    export default {
-        name: "ModuleList",
-        components: {ModuleFilters, Module},
-        data: () => ({
-            page: 1,
-            pageSize: 10,
-            pageSizes: [5, 10, 15, 20, 25, 50, 100],
-            sort: ["Title Ascending"],
-            sortOptions: ["Title Ascending","Title Descending"],
-            filter: {},
-        }),
-        methods: {
-            filterChanged(newVal) {
-                this.filter = newVal
-            }
-        }
+export default {
+  name: "ModuleList",
+  components: {ModuleFilters, Module},
+  data: () => ({
+    page: 1,
+    pageSize: 10,
+    pageSizes: [5, 10, 15, 20, 25, 50, 100],
+    sort: ["Title Ascending"],
+    sortOptions: ["Title Ascending", "Title Descending"],
+    filter: "",
+  }),
+  computed: {
+    offset: function () {
+      return (this.page * this.pageSize) - this.pageSize
+    },
+  },
+  methods: {
+    filterChanged(newVal) {
+      this.filter = newVal
     }
+  }
+}
 </script>
 
 <style scoped>
--- client/src/views/ModulePage.vue
+++ client/src/views/ModulePage.vue
@@ -1,21 +1,21 @@
 <template>
-    <ApolloQuery :query="gql => gql`query ModuleByCode($moduleCode: String!) {
-  moduleOne(filter: { _id: $moduleCode }) {
-    _id
-    module_title
+  <ApolloQuery :query="gql => gql`query modules($moduleCode: String!) {
+  modules(where: {code: {_eq: $moduleCode}}) {
+    code
+    title
     scqf_level
     scqf_credit_value
     ects_credit_value
-    module_leader {
+    staff_module_leader {
       name
       email
     }
     school
     subject_area_group
-    prerequisites
-    module_content_description
-    learning_outcomes
-    reading_list
+    prerequisites_html
+    module_content_description_html
+    learning_outcomes_html
+    reading_list_html
     teaching_instances {
       term
       trimester
@@ -23,214 +23,193 @@
       delivery_mode
       delivery_location
       partner
-      delivery_staff_member {
+      staff_delivery_staff_member {
         name
         email
       }
-      module_organiser {
+      staff_module_organiser {
         name
         email
       }
-      lta_approach
-      formative_assessment
-      summative_assessment
-      student_activity {
-        activities {
+      lta_approach_html
+      formative_assessment_html
+      summative_assessment_html
+      student_activities {
           mode
           type
           study_hours
-        }
-        total_study_hours
-        expected_study_hours
       }
-      assessment {
-        assessments {
-          assessment_type
+      total_study_hours
+      expected_total_study_hours
+      assessments {
+          type
           weighting
           lo_covered
           week_due
           length_hours
           length_words
-        }
-        component1_subtotal
-        component2_subtotal
-        module_subtotal
       }
+      component1_subtotal
+      component2_subtotal
+      module_subtotal
     }
   }
 }
 `"
-                 :variables="{ moduleCode: moduleCode }"
-                 :update="data=> data.moduleOne">
-        <template slot-scope="{ result: { data, loading } }">
-            <div v-if="loading">
-                <v-container>
-                    <v-row
-                            align="center"
-                            justify="center">
-                        <v-col
-                                cols="12"
-                                class="text-center">
-                            <v-progress-circular
-                                    indeterminate
-                                    color="primary"
-                                    size="50"></v-progress-circular>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </div>
-            <div v-else-if="data">
-                <v-container>
-                    <v-row>
-                        <h2>{{ data.module_title }}</h2>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="12"
-                        md="6">
-                            <v-row>
-                                <v-col cols="12">
-                                    <v-card
-                                            class="mx-auto">
-                                        <v-card-title>
-                                            <h5>Module Details</h5>
-                                        </v-card-title>
-                                        <v-card-text>
-                                            <v-simple-table>
-                                                <tbody>
-                                                <tr v-for="(item, i) in moduleDetailColumns" :key="i">
-                                                    <td>
-                                                        {{item.label}}
-                                                    </td>
-                                                    <td>
-                                                        <div v-html="item.field(data)"></div>
-                                                    </td>
-                                                </tr>
-                                                </tbody>
-                                            </v-simple-table>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col cols="12">
-                                    <v-card>
-                                        <v-tabs v-model="tab"
-                                                centered>
-                                            <v-tab>
-                                                Prerequisites
-                                            </v-tab>
-                                            <v-tab>
-                                                Description of module content
-                                            </v-tab>
-                                            <v-tab>
-                                                Learning Outcomes for module
-                                            </v-tab>
-                                            <v-tab v-if="data.reading_list.length > 0">
-                                                Reading List
-                                            </v-tab>
-                                        </v-tabs>
-                                        <v-tabs-items v-model="tab">
-                                            <v-tab-item>
-                                                <v-card>
-                                                    <v-card-text>
-                                                        <template v-for="line in data.prerequisites">
-                                                            {{line}}<br v-bind:key="line">
-                                                        </template>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-tab-item>
-                                            <v-tab-item>
-                                                <v-card flat>
-                                                    <v-card-text>
-                                                        <template v-for="line in data.module_content_description">
-                                                            {{line}}<br v-bind:key="line">
-                                                        </template>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-tab-item>
-                                            <v-tab-item>
-                                                <v-card flat>
-                                                    <v-card-text>
-                                                        <template v-for="line in data.learning_outcomes">
-                                                            {{line}}<br v-bind:key="line">
-                                                        </template>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-tab-item>
-                                            <v-tab-item v-if="data.reading_list.length > 0">
-                                                <v-card flat>
-                                                    <v-card-text>
-                                                        <template v-for="line in data.reading_list">
-                                                            {{line}}<br v-bind:key="line">
-                                                        </template>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-tab-item>
-                                        </v-tabs-items>
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                        </v-col>
-                        <v-col
-                                cols="12"
-                        md="6">
-                            <v-row>
-                                <v-col cols="12">
-<!--                                    <v-card>-->
-<!--                                        <v-card-text>-->
-                                            <v-expansion-panels
-                                                    multiple
-                                            >
-                                                <v-expansion-panel
-                                                        v-for="(instance, i) in data.teaching_instances"
-                                                        :key="i"
-                                                >
-                                                    <v-expansion-panel-header>
-                                                        {{instance.term}}, {{instance.trimester}},
-                                                        {{instance.delivery_mode}},
-                                                        {{instance.partner}}
-                                                    </v-expansion-panel-header>
-                                                    <v-expansion-panel-content>
-                                                        <ModuleTeachingInstance
-                                                                v-bind:instance="instance"></ModuleTeachingInstance>
-                                                    </v-expansion-panel-content>
-                                                </v-expansion-panel>
-                                            </v-expansion-panels>
-<!--                                        </v-card-text>-->
-<!--                                    </v-card>-->
-                                </v-col>
-                            </v-row>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </div>
-        </template>
-    </ApolloQuery>
+               :variables="{ moduleCode: moduleCode }"
+               :update="data=> data.modules[0]">
+    <template slot-scope="{ result: { data, loading } }">
+      <div v-if="loading">
+        <v-container>
+          <v-row
+              align="center"
+              justify="center">
+            <v-col
+                cols="12"
+                class="text-center">
+              <v-progress-circular
+                  indeterminate
+                  color="primary"
+                  size="50"></v-progress-circular>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+      <div v-else-if="data">
+        <v-container>
+          <v-row>
+            <h2>{{ data.title }}</h2>
+          </v-row>
+          <v-row>
+            <v-col cols="12"
+                   md="6">
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                      class="mx-auto">
+                    <v-card-title>
+                      <h5>Module Details</h5>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-simple-table>
+                        <tbody>
+                        <tr v-for="(item, i) in moduleDetailColumns" :key="i">
+                          <td>
+                            {{ item.label }}
+                          </td>
+                          <td v-html="item.field(data)">
+                          </td>
+                        </tr>
+                        </tbody>
+                      </v-simple-table>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-card>
+                    <v-tabs v-model="tab"
+                            centered>
+                      <v-tab v-if="data.prerequisites_html">
+                        Prerequisites
+                      </v-tab>
+                      <v-tab v-if="data.module_content_description_html">
+                        Description of module content
+                      </v-tab>
+                      <v-tab v-if="data.learning_outcomes_html">
+                        Learning Outcomes for module
+                      </v-tab>
+                      <v-tab v-if="data.reading_list_html">
+                        Reading List
+                      </v-tab>
+                    </v-tabs>
+                    <v-tabs-items v-model="tab">
+                      <v-tab-item v-if="data.prerequisites_html">
+                        <v-card>
+                          <v-card-text v-html="data.prerequisites_html"></v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item v-if="data.module_content_description_html">
+                        <v-card flat>
+                          <v-card-text v-html="data.module_content_description_html"></v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item v-if="data.learning_outcomes_html">
+                        <v-card flat>
+                          <v-card-text v-html="data.learning_outcomes_html"></v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item v-if="data.reading_list_html">
+                        <v-card flat>
+                          <v-card-text v-html="data.reading_list_html"></v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                    </v-tabs-items>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col
+                cols="12"
+                md="6">
+              <v-row>
+                <v-col cols="12">
+                  <!--                                    <v-card>-->
+                  <!--                                        <v-card-text>-->
+                  <v-expansion-panels
+                      multiple
+                  >
+                    <v-expansion-panel
+                        v-for="(instance, i) in data.teaching_instances"
+                        :key="i"
+                    >
+                      <v-expansion-panel-header>
+                        {{ instance.term }}, {{ instance.trimester }},
+                        {{ instance.delivery_mode }},
+                        {{ instance.partner }}
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <ModuleTeachingInstance
+                            v-bind:instance="instance"></ModuleTeachingInstance>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                  <!--                                        </v-card-text>-->
+                  <!--                                    </v-card>-->
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </template>
+  </ApolloQuery>
 </template>
 
 <script>
-    import ModuleTeachingInstance from "../components/Modules/ModuleTeachingInstance";
+import ModuleTeachingInstance from "../components/Modules/ModuleTeachingInstance";
 
-    export default {
-        name: "ModulePage",
-        components: {ModuleTeachingInstance},
-        props: ['moduleCode'],
-        data: () => ({
-            tab: null,
-            module: {},
-            moduleDetailColumns: [{label: 'Module Title:', field: item => item.module_title},
-                {label: 'Module Code:', field: item => item._id},
-                {
-                    label: 'Module Leader',
-                    field: item => `<a :href="${item.module_leader.email}">${item.module_leader.name}</a>`
-                },
-                {label: 'School:', field: item => item.school},
-                {label: 'Subject area group:', field: item => item.subject_area_group},
-                {label: 'SCQF Level:', field: item => item.scqf_level},
-                {label: 'SCQF Credit Value:', field: item => item.scqf_credit_value},
-                {label: 'ECTS Credit Value:', field: item => item.ects_credit_value}],
-        }),
-    }
+export default {
+  name: "ModulePage",
+  components: {ModuleTeachingInstance},
+  props: ['moduleCode'],
+  data: () => ({
+    tab: null,
+    module: {},
+    moduleDetailColumns: [{label: 'Module Title:', field: item => item.title},
+      {label: 'Module Code:', field: item => item.code},
+      {
+        label: 'Module Leader',
+        field: item => `<a :href="${item.staff_module_leader.email}">${item.staff_module_leader.name}</a>`
+      },
+      {label: 'School:', field: item => item.school},
+      {label: 'Subject area group:', field: item => item.subject_area_group},
+      {label: 'SCQF Level:', field: item => item.scqf_level},
+      {label: 'SCQF Credit Value:', field: item => item.scqf_credit_value},
+      {label: 'ECTS Credit Value:', field: item => item.ects_credit_value}],
+  }),
+}
 </script>
 
 <style scoped>
